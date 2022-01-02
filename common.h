d10fa83f5162302a6a6305abc6cc98dcb0473b2c
Georg@Atlas <gthb@filios.de>
2022-01-02 19:44:12 +0100

    Implmented Patt's parser for expression /

--- common.h
+++ common.h
@@ -8,4 +8,5 @@
 #include <stddef.h>
 #include <stdint.h>
 #define DEBUG_TRACE_EXECUTION
+#define DEBUG_PRINT_CODE
 #endif //CLOX_COMMON_H
--- compiler.h
+++ compiler.h
@@ -4,7 +4,8 @@
 
 #ifndef CLOX_COMPILER_H
 #define CLOX_COMPILER_H
+#include "vm.h"
 
-void compile(const char* source);
+bool compile(const char* source, Chunk* chunk);
 
 #endif //CLOX_COMPILER_H
